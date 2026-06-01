c80d9dcc2f59a136e9ac954a83e1161d23b6b884
Je1son00 <jeison_amparo@ucne.edu.do>
2026-05-31 23:09:35 -0400

    Crear metodo registrar Profesor y metodo registrar materia

--- src/logica/GestorAcademico.java
+++ src/logica/GestorAcademico.java
@@ -3,6 +3,8 @@ package logica;
 import java.util.ArrayList;
 import java.util.Scanner;
 import clases.Estudiante;
+import clases.Materia;
+import clases.Profesor;
 
 public class GestorAcademico {
 
@@ -29,6 +31,39 @@ public class GestorAcademico {
 
         estudiantes.add(new Estudiante(nombre, apellido, matricula, edad, carrera, fechaInscripcion));
         System.out.println("\nEstudiante registrado correctamente.");
+    }
+
+    public static void registrarProfesor(Scanner sc, ArrayList<Profesor> profesores) {
+        sc.nextLine();
+        System.out.print("\nIngrese el codigo del profesor: ");
+        String codigo = sc.nextLine();
+
+        System.out.print("Ingrese el nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Ingrese el apellido: ");
+        String apellido = sc.nextLine();
+
+        System.out.print("Ingrese la especialidad: ");
+        String especialidad = sc.nextLine();
+
+        profesores.add(new Profesor(nombre, apellido, codigo, especialidad));
+        System.out.println("\nProfesor registrado correctamente.");
+    }
+
+    public static void registrarMateria(Scanner sc, ArrayList<Materia> materias) {
+        sc.nextLine();
+        System.out.print("\nIngrese el codigo de la materia: ");
+        String codigo = sc.nextLine();
+
+        System.out.print("Ingrese el nombre de la materia: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Ingrese cantidad de creditos: ");
+        int creditos = sc.nextInt();
+
+        materias.add(new Materia(codigo, nombre, creditos));
+        System.out.println("\nMateria registrada correctamente.");
     }
 
 }
